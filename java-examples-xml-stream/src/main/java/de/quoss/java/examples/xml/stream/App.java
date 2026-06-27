b8c17022ab07ca08403717bd9a29554eaffaafa0
cquoss <clemens@quoss.de>
2026-06-27 12:12:09 +0200

    xml stream

---
+++ java-examples-xml-stream/src/main/java/de/quoss/java/examples/xml/stream/App.java
@@ -0,0 +1,11 @@
+package de.quoss.java.examples.xml.stream;
+
+public class App {
+
+    // public api methods
+    
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+    
+}
